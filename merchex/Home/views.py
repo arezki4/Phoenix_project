2f04cb7156cc05dd1ee10d770dc1113f831b5528
arezki4 <djaroudarezki@gmail.com>
2023-12-07 19:36:59 +0100

    add a new page for testing the rooting with django

--- merchex/Home/views.py
+++ merchex/Home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello User!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
